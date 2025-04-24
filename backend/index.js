fe65c74f4210b87dbe6302330faf68a4d0c39eca
Yash Gorasia <yash.gorasia118198@marwadiuniversity.ac.in>
2025-04-24 16:27:31 +0530

    fixed deployed link

--- backend/index.js
+++ backend/index.js
@@ -12,11 +12,12 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
     cors: {
-        origin: "http://localhost:5173", // Change to your client URL
+        origin: ["http://localhost:5173", "https://coide.vercel.app"],
         methods: ["GET", "POST"],
         credentials: true
     }
 });
+
 
 const userMap = {};
 const roomCode = {};
