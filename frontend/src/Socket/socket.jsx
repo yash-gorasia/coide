f17047816297e8fcb3e700184b25d6bb66f686f6
Yash Gorasia <yash.gorasia118198@marwadiuniversity.ac.in>
2025-04-24 23:08:26 +0530

    socket secure: true, frontend

--- frontend/src/Socket/socket.jsx
+++ frontend/src/Socket/socket.jsx
@@ -6,6 +6,7 @@ export const initSocket = async () => {
         reconnectionAttempt: 'Infinity',
         timeout: 10000,
         transports: ['websocket'],
+        secure: true,
     };
     return io(import.meta.env.VITE_WS_URL, options);
 }
